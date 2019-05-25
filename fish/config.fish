3cf00dafbae364554d2873576338152d1442cd43
Taishi Nakashima <taishi.m.and.p@gmail.com>
2019-05-25 17:18:41 +0900

    [Fix] fish non-ascii code fonts

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡︎'
+set __fish_git_prompt_char_dirtystate '☠︎ '
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_untrackedfiles '⚠︎ '
 set __fish_git_prompt_char_stashstate '↩ '
